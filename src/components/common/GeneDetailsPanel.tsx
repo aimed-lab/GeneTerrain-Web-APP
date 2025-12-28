47eb951c975d87538f2a1a3606e1f8a119c726a6
Geetanjali Oishe <geetanjali@Geetanjalis-MacBook-Pro.local>
2025-12-27 19:44:25 -0600

    api secret removed

--- src/components/common/GeneDetailsPanel.tsx
+++ src/components/common/GeneDetailsPanel.tsx
@@ -600,9 +600,6 @@ export default function GeneDetailsPanel({ selectedGene, allPoints, connected = 
   }, [selectedGene?.geneName]);
 
   const getGeneDetails = async () => {
-    console.log("genedetails called");
-    // const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-    // const apiKey = "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";
     const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
     if (!apiKey) return;
     setIsLoading(true);
